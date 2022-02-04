4c33d311bb6ba1a6f4b106bb3b1c1ec1558df803
josemaSab <j.saborido@josemasaborido.es>
2022-02-04 15:13:12 +0100

    completando clase transacción. Creaccion de clases de calculos para operar con bytes[] y strings

---
+++ LuLeCoin/Calculos/CalculosByteString.cs
@@ -0,0 +1,20 @@
+﻿namespace LuLeCoin.Calculos
+{
+    public static class CalculosByteString
+    {
+        public static byte[] stringToArrayBytes(string cadena)
+        {
+            byte[] array = new byte[0];
+            array = Encoding.ASCII.GetBytes(cadena);
+            return array;
+
+        }
+
+        public static string arrayBytesToString(byte[] array)
+        {
+            string cadena = null;
+            cadena = Encoding.ASCII.GetString(array);
+            return cadena;
+        }
+    }
+}
--- LuLeCoin/Modelos/Transacciones/Transaccion.cs
+++ LuLeCoin/Modelos/Transacciones/Transaccion.cs
@@ -1,5 +1,6 @@
 ﻿using LuLeCoin.Calculos;
-using System.Runtime.Serialization.Formatters.Binary;
+using LuLeCoin.Encriptacion;
+
 
 namespace LuLeCoin.Modelos.Transacciones
 {
@@ -34,23 +35,60 @@ namespace LuLeCoin.Modelos.Transacciones
             Firma = firma;
             Cantidad = cantidad;
             TimeStamp = CalculosFecha.calculaMilisegundos(CalculosFecha.ahora);
+            Hash = calculaHashTransaccion();
         }
 
         //METODOS
         /**
          * Metodo que transforma el objeto transacción en un array de bytes
          * */
-        public byte[] getContenidoTransaccion(Transaccion transaccion)
+        public byte[] getContenidoTransaccion()
         {
-            if (transaccion == null)
-                return null;
-            BinaryFormatter bf = new BinaryFormatter();
-            using (MemoryStream ms = new MemoryStream())
-            {
-                bf.Serialize(ms, transaccion);
-                return ms.ToArray();
-            }
+            byte[] contenido;
+            StringBuilder sb = new StringBuilder();
 
+            
+            if(this == null)
+            {
+                return null;
+            }
+            //Pasamos a string los datos de la transaccion
+            sb.Append(CalculosByteString.arrayBytesToString(this.Emisor));
+            sb.Append(CalculosByteString.arrayBytesToString(this.Destinatario));
+            sb.Append(CalculosByteString.arrayBytesToString(this.Firma));
+            sb.Append(CalculosByteString.arrayBytesToString(BitConverter.GetBytes(this.Cantidad)));
+            sb.Append(CalculosByteString.arrayBytesToString(BitConverter.GetBytes(this.TimeStamp)));
+            contenido = Encoding.ASCII.GetBytes(sb.ToString());
+            return contenido;
         }
+
+        /**
+         * Calcula el hash de la transacción y pasa a ser el identificador hash
+         */
+
+        public byte[] calculaHashTransaccion()
+        {
+            return HashearSHA256.calculoHash(getContenidoTransaccion());          
+        }
+
+        /**
+         * Metodo que muestra una cadena de caracteres con los datos de la transacción.
+         */
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("TRANSACCION\n");
+            sb.Append("-----------\n");
+            sb.Append($"Hash: {HashearSHA256.pasarArrayByteString(this.Hash)}\n");
+            sb.Append($"Emisor: {HashearSHA256.pasarArrayByteString(this.Emisor)}\n");
+            sb.Append($"Destinatario: {HashearSHA256.pasarArrayByteString(this.Destinatario)}\n");
+            sb.Append($"Cantidad: {this.Cantidad}\n");
+            sb.Append($"Fecha Transacción: {this.TimeStamp}\n");
+            return sb.ToString();
+        }
+
+        //TODO Validar hash 
+
+        //TODO Validar transaccion
     }
 }
