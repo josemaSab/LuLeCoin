7e11f152e054f9e0ae93fd434839e9425e5e91a6
josemaSab <j.saborido@josemasaborido.es>
2022-02-11 20:31:21 +0100

    Interfaz genrica de Listener

---
+++ LuLeCoin/RedP2P/Servicios/Interfaces/GenericListener.cs
@@ -0,0 +1,18 @@
+﻿namespace LuLeCoin.RedP2P.Servicios.Interfaces
+{
+    /**
+     * Interface generica que difine los metodos basicos para los
+     * escuchadores.
+     */
+    public interface GenericListener<T>
+    {
+        /**
+         * Metodo que añade un escuchador.
+         */
+        void addListener(T objeto);
+        /**
+         * Metodo que elimina a un escuchador
+         */
+        void removeListener(T objeto);
+    }
+}
